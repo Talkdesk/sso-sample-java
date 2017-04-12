d4e1c98b2ce3a093906f2641429afca022730d4a
João Carvalho <joaopecarvalho@gmail.com>
2017-04-12 15:21:36 +0100

    Initial commit
    This project was generated by https://start.spring.io, including the
    'Cloud OAuth2' dependency.

---
+++ src/main/java/com/talkdesk/sample/sso/SsoSampleJavaApplication.java
@@ -0,0 +1,12 @@
+package com.talkdesk.sample.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoSampleJavaApplication.class, args);
+	}
+}
